5a8fb9fdd9670ed560d50d3e3fa006349736d950
tk_hirom <takahiroinkyoto@gmail.com>
2023-10-18 21:53:39 +0900

    add explain

--- kotlin/recursion/getTotalByRecursion.kts
+++ kotlin/recursion/getTotalByRecursion.kts
@@ -7,6 +7,7 @@ fun sum(numbers: MutableList<Int>): Int {
     return if (numbers.size == 0) {
         0
     } else {
+        // Call recursively ths 'sum' function with 'numbers' one element shorter than the original and add result and the first element
         numbers.removeAt(0) + sum(numbers)
     }
 }
